c04b9db87b59cdb7adb9a79f1bbad1f90db9faa5
vaibhvbh77 <69358954+vaibhvbh77@users.noreply.github.com>
2022-01-25 18:23:15 +0530

    Time: 1 ms (100.00%), Space: 39.9 MB (82.63%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.java
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -1,32 +1,17 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
         
-        
-//        1iteration finding the max element 
-        int max=0;
-        
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>arr[max])
-                max=i;
+        int i=0;
+        while(i+1<arr.length&&arr[i]<arr[i+1]){
+            i++;
         }
-        
-        if(arr[max]==arr[0]||arr[max]==arr[arr.length-1])
+        if(i==0||i==arr.length-1)
             return false;
-        for(int i=0;i<max;i++){
-            if(arr[i]<arr[i+1])
-                continue;
-            else
-                return false;
+        while(i+1<arr.length&&arr[i]>arr[i+1])
+        {
+            i++;
         }
-         for(int i=max;i<arr.length-1;i++){
-            if(arr[i]>arr[i+1])
-                continue;
-            else
-                return false;
-        }
-        return true;
-        
-        
+        return i==arr.length-1?true:false;
         
     }
 }

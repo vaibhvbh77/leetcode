babef1ba50ee1dd67fae25f531491ef2aa8a3293
vaibhvbh77 <69358954+vaibhvbh77@users.noreply.github.com>
2022-03-23 18:56:49 +0530

    Added solution - LeetHub

---
+++ 5. Max and Second Max - GFG/5.-max-and-second-max.java
@@ -0,0 +1,74 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+        
+
+
+// Solution class to implement function largestAndSecondLargest
+class Solution{
+    
+    // Function to find largest and second largest element in the array
+    public static ArrayList<Integer> largestAndSecondLargest(int sizeOfArray, int arr[])
+    {
+        ArrayList<Integer>li=new ArrayList<Integer>();
+       int l=arr[0];
+       int second=-1;
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>l)
+            {
+                second=l;
+                l=arr[i];
+                
+            }
+             if(arr[i]>second&&arr[i]!=l)
+            second=arr[i];
+        }
+        li.add(l);
+        li.add(second);
+        
+        return li;
+        
+        
+    }
+}
+
+// { Driver Code Starts.
+
+// Driver class
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    int sizeOfArray = Integer.parseInt(br.readLine());
+		    
+		    int arr[] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int index = 0;index < sizeOfArray; index++){
+		        arr[index] = Integer.parseInt(elements[index]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		    res = obj.largestAndSecondLargest(sizeOfArray, arr);
+		    System.out.println(res.get(0) + " " + res.get(1) );
+		}
+	}
+}  // } Driver Code Ends

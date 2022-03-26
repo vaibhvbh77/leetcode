a3e46448b70e2e3dd9768fa30c834f1b04c6e06e
vaibhvbh77 <69358954+vaibhvbh77@users.noreply.github.com>
2022-03-26 20:40:26 +0530

    Time: 2 ms (6.40%), Space: 54.3 MB (51.69%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,11 +1,11 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length==1){
-            if(target==nums[0])
-                return 0;
-            else
-                return -1;
-        }
+        // if(nums.length==1){
+        //     if(target==nums[0])
+        //         return 0;
+        //     else
+        //         return -1;
+        // }
         
         
         

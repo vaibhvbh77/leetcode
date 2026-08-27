845e0c006fecdfb9addbe16c4da4f69801cdb810
vaibhvbh77 <69358954+vaibhvbh77@users.noreply.github.com>
2026-08-28 01:24:33 +0530

    Time: 2 ms (92.54%), Space: 44.9 MB (67.16%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
@@ -1,121 +1,61 @@
 class Solution {
     public String lexGreaterPermutation(String s, String target) {
 
-        int freq[] = new int[26];
+        int[] freq = new int[26];
 
-        // Count characters in s
         for (char c : s.toCharArray()) {
             freq[c - 'a']++;
         }
 
         StringBuilder ans = new StringBuilder();
 
-        int i = 0;
-
-        // Go left to right
-        while (i < target.length()) {
+        // Match target from left to right
+        for (int i = 0; i < target.length(); i++) {
 
             int x = target.charAt(i) - 'a';
 
-            // 1. Same character is available
+            // Same character available
             if (freq[x] > 0) {
-
-                freq[x]--;
                 ans.append(target.charAt(i));
-                i++;
+                freq[x]--;
+                continue;
             }
 
-            // 2. Same character is NOT available
-            else {
-
-                // Try smallest character greater than target[i]
-                int greater = -1;
-
-                for (int j = x + 1; j < 26; j++) {
-                    if (freq[j] > 0) {
-                        greater = j;
-                        break;
-                    }
-                }
-
-                // Found a greater character
-                if (greater != -1) {
-
-                    ans.append((char)('a' + greater));
-                    freq[greater]--;
-
-                    // Put remaining characters in sorted order
-                    for (int j = 0; j < 26; j++) {
-                        while (freq[j] > 0) {
-                            ans.append((char)('a' + j));
-                            freq[j]--;
-                        }
-                    }
-
-                    return ans.toString();
-                }
-
-                // No greater character.
-                // Backtrack to previous matched position.
-                while (ans.length() > 0) {
-
-                    i--;
-
-                    char last = ans.charAt(ans.length() - 1);
-                    ans.deleteCharAt(ans.length() - 1);
-
-                    // Restore the character
-                    freq[last - 'a']++;
-
-                    int y = target.charAt(i) - 'a';
-
-                    // Try to bump this position
-                    for (int j = y + 1; j < 26; j++) {
-
-                        if (freq[j] > 0) {
-
-                            ans.append((char)('a' + j));
-                            freq[j]--;
-
-                            // Fill remaining characters smallest first
-                            for (int k = 0; k < 26; k++) {
-                                while (freq[k] > 0) {
-                                    ans.append((char)('a' + k));
-                                    freq[k]--;
-                                }
-                            }
-
-                            return ans.toString();
-                        }
-                    }
-                }
-
-                return "";
-            }
+            // Can't match -> try to make this position bigger
+            return build(ans, freq, target, i);
         }
 
-        // We matched the whole target exactly.
-        // But we need STRICTLY greater, so backtrack.
-        while (ans.length() > 0) {
+        // Matched entire target.
+        // Need to make it STRICTLY greater.
+        return build(ans, freq, target, target.length() - 1);
+    }
 
-            i--;
+    private String build(StringBuilder ans, int[] freq,
+                         String target, int i) {
 
-            char last = ans.charAt(ans.length() - 1);
-            ans.deleteCharAt(ans.length() - 1);
-            freq[last - 'a']++;
+        while (i >= 0) {
 
+            // Restore the character at this position
+            if (ans.length() > i) {
+                char c = ans.charAt(i);
+                freq[c - 'a']++;
+                ans.deleteCharAt(i);
+            }
+
+            // Find smallest character > target[i]
             int x = target.charAt(i) - 'a';
 
             for (int j = x + 1; j < 26; j++) {
 
                 if (freq[j] > 0) {
 
-                    ans.append((char)('a' + j));
+                    ans.append((char) ('a' + j));
                     freq[j]--;
 
+                    // Remaining characters smallest first
                     for (int k = 0; k < 26; k++) {
                         while (freq[k] > 0) {
-                            ans.append((char)('a' + k));
+                            ans.append((char) ('a' + k));
                             freq[k]--;
                         }
                     }
@@ -123,6 +63,10 @@ class Solution {
                     return ans.toString();
                 }
             }
+
+            // Can't increase this position.
+            // Go one position left.
+            i--;
         }
 
         return "";

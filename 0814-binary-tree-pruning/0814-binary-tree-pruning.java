6fc1315c2aee5fa2da613d28c0af0643bb285854
vaibhvbh77 <69358954+vaibhvbh77@users.noreply.github.com>
2026-09-04 18:13:44 +0530

    Time: 0 ms (100%), Space: 42.9 MB (65.7%) - LeetHub

---
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+
+         return   solve(root);
+    }
+    public TreeNode solve(TreeNode root){
+        if(root==null) return null;
+
+
+        if(!containsOne(root.left)){
+            root.left=null;
+        }
+          if(!containsOne(root.right)){
+            root.right=null;
+        }
+
+                if(root.val == 0 && root.left == null && root.right == null)
+            return null;
+
+        solve(root.left);
+        solve(root.right);
+
+        return root;
+
+
+
+    }
+
+    public boolean containsOne(TreeNode root){
+        if(root==null) return false;
+        if(root.val==1) return true;
+
+        boolean left=containsOne(root.left);
+        boolean right=containsOne(root.right);
+
+        if(left || right) return true;
+
+        return false;
+    }
+}

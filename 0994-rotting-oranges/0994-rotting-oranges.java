9717f39b6c6df60910dc3cdd611ee1755b0a5758
vaibhvbh77 <69358954+vaibhvbh77@users.noreply.github.com>
2026-09-11 17:05:50 +0530

    Time: 2 ms (88.29%), Space: 44 MB (84.1%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,60 +1,62 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<int[]>q=new ArrayDeque<>();
-        int minutes=0;
-        int fresh=0;
-
+        Queue<int []>q=new ArrayDeque<>();
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==2){
                     q.offer(new int[]{i,j});
                 }
-                if(grid[i][j]==1) fresh++;
             }
         }
+        int minutes=0;
+
         while(q.isEmpty()==false){
-            // 
-
             int size=q.size();
-
             for(int i=0;i<size;i++){
+                int []curr=q.poll();
+                int row=curr[0];
+                int col=curr[1];
 
-            int []curr=q.poll();
-            int row=curr[0];
-            int col=curr[1];
                 // up
                 if(row-1>=0 && grid[row-1][col]==1){
                     grid[row-1][col]=2;
-                    fresh--;
                     q.offer(new int[]{row-1,col});
                 }
 
                         // down
                 if(row+1<grid.length && grid[row+1][col]==1){
                     grid[row+1][col]=2;
-                    fresh--;
                     q.offer(new int[]{row+1,col});
                 }
 
                        // right
                 if(col+1<grid[0].length && grid[row][col+1]==1){
                     grid[row][col+1]=2;
-                    fresh--;
                     q.offer(new int[]{row,col+1});
                 }
 
                         // left
                 if(col-1>=0 && grid[row][col-1]==1){
                     grid[row][col-1]=2;
-                    fresh--;
                     q.offer(new int[]{row,col-1});
                 }
             }
-              if(!q.isEmpty()){
+             if(!q.isEmpty()){
                 minutes++;
             }
+// 
         }
 
-        return fresh==0?minutes:-1;
+         for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+        return minutes;
+
+
+        
     }
 }
